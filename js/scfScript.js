f3474357e1645ab2917642e78d688eeee6f88f51
jmlodge <jm-lodge@hotmail.co.uk>
2016-10-05 19:53:47 +0100

    added moduals fixed target _self

--- js/scfScript.js
+++ js/scfScript.js
@@ -1,15 +1,19 @@
 $(document).ready(function(){
 
- 	$(document).ready(function() {
+ 	
 
    	var docHeight = $(window).height();
    	var footerHeight = $('#footerBottom').height();
    	var footerTop = $('#footerBottom').position().top + footerHeight;
 
    	if (footerTop < docHeight) {
-    $('#footerBottom').css('margin-top', (docHeight - footerTop * 12) + 'px');
+    $('#footerBottom').css('margin-top', (docHeight - footerTop * 10) + 'px');
    	}
-  });
+ 
+ 	$('a[href*=services]').on('click', function(event){     
+	    event.preventDefault();
+	    $('html,body').animate({scrollTop:$(this, "#scrollTo").offset().top}, 500);
+	});
    
     //$(".serviceSlide").hover(function(){
         //$(this).find("div.serviceText").slideDown("slow");
